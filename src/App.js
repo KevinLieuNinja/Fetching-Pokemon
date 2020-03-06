6919c31aa7c1c5ef448f95b66ad0f5d350f5e4d7
KevinLieuNinja <klieu925@gmail.com>
2020-03-06 15:29:18 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state ={
+      items:[],
+      isLoaded: false,
+    }
+  }
+
+  fetchPokemon =() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=807")
+      .then(res=> res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json.results,
+        })
+      });
+  }
+
+  render(){
+    var { isLoaded, items } = this.state;
+    
+    if (!isLoaded){
+      return (
+        <div>
+          <button onClick={this.fetchPokemon}>Fetching pokemon</button>
+        </div>
+        )
+    }
+    else {
+      return (
+        <div className="App">
+          
+          <ul>
+            {items.map(item => (
+              <p key={items.id}> {item.name} </p>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+}   
 export default App;
